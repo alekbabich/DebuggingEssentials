d568302201cfc7d4f38cbd3bcc2f268839c16e90
ababich <ababich@bottomline.com>
2021-02-15 13:28:25 -0500

    Initial commit of simple enless loop app for debug play

---
+++ src/com/babich/debug/Endless.java
@@ -0,0 +1,25 @@
+package com.babich.debug;
+
+import java.io.IOException;
+
+public class Endless {
+    public static void main(String[] args) throws IOException {
+        while (true) {
+            int read = System.in.read();
+            System.out.println("Input: " + read);
+            if (filter(read)) {
+                process(read);
+            }
+        }
+    }
+
+    private static void process(int read) {
+        if (Math.max(read, 90) % 2 == 0) {
+            System.out.println("!");
+        }
+    }
+
+    private static boolean filter(int read) {
+        return read != '\n' && read != 'a';
+    }
+}
